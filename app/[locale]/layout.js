313248e294cb63093514feb00c63e83791b8575a
guille garcia <ggarciacardiel@gmail.com>
2024-12-21 21:48:54 +0100

    adds og-image

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -26,6 +26,16 @@ export default async function LocaleLayout({ children, params: { locale } }) {
         <link rel="icon" href="/favicon.ico" />
         <title>Carlota Vilamala</title>
         <meta name="description" content="Portfolio & Works" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://carlotavilamala.vercel.app/" />
+        <meta property="og:title" content="Carlota Vilamala" />
+        <meta property="og:description" content="Fashion Stylist & Creative Direction" />
+        <meta property="og:image" content="https://carlotavilamala.vercel.app/og-image.jpg" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://carlotavilamala.vercel.app/" />
+        <meta property="twitter:title" content="Carlota Vilamala" />
+        <meta property="twitter:description" content="Fashion Stylist & Creative Direction" />
+        <meta property="twitter:image" content="https://carlotavilamala.vercel.app/og-image.jpg" />
       </head>
       <body className={`${playfair.className} antialiased min-h-screen flex flex-col`}>
         <NextIntlClientProvider locale={locale} messages={messages}>
