7dc73e3e4085d2e5005f4aa9a4695a6af1e4a1ce
guille garcia <ggarciacardiel@gmail.com>
2024-12-21 19:30:16 +0100

    updates rights

--- app/[locale]/components/Footer.js
+++ app/[locale]/components/Footer.js
@@ -6,15 +6,21 @@ export default function Footer() {
   const t = useTranslations('footer');
 
   return (
-    <footer className="py-8 bg-[#faf9f6] mt-auto">
-      <div className="text-xs relative">
-        {/* Copyright and Rights */}
+    <footer className="py-8 mb-4 bg-[#faf9f6] mt-auto">
+      <div className="text-[12px] relative">
         <div className="flex flex-col md:flex-row md:justify-between gap-2 md:gap-0">
           <div className="text-center md:text-left md:absolute md:left-8">
-            © {new Date().getFullYear()} Carlota Vilamala
+            © 2025 Carlota Vilamala - {t('rights')}
           </div>
           <div className="text-center md:text-right md:absolute md:right-8">
-            {t('rights')}
+            <a 
+              href="https://guillegarciac.github.io/mycv/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-red-500 transition-colors"
+            >
+              {t('designed')}
+            </a>
           </div>
         </div>
       </div>
