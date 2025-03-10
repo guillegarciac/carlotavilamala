f18b5d891a1f0739251f7ded0dd1edf60d1f5027
guille garcia <ggarciacardiel@gmail.com>
2025-03-10 21:00:10 +0100

    adds new visuals 2025

--- app/[locale]/data/visuals.js
+++ app/[locale]/data/visuals.js
@@ -1,4 +1,4 @@
-export const visuals = Array.from({ length: 15 }, (_, i) => ({
+export const visuals = Array.from({ length: 18 }, (_, i) => ({
   id: `visual${i + 1}`,
   imageUrl: `/visuals/image${String(i + 1).padStart(5, '0')}.jpg`,
   detailImages: [] // Visuals don't have detail images
